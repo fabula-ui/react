a4428e4a6678671304c9106996a313e012837b56
Bruno Poeta <bruno.poeta@toptal.com>
2020-05-26 21:51:33 -0300

    Styling toast component

--- src/Examples/toast.jsx
+++ src/Examples/toast.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useContext } from 'react';
 
-import { Button, ToastService } from '@fabula/react-dev';
+import { Button, Toast, ToastService } from '@fabula/react-dev';
 
 const ToastExample = props => {
     const { test } = props;
@@ -13,16 +13,24 @@ const ToastExample = props => {
     }, []);
 
     const addToast = () => {
-        // toastService.showToast({
-        //     message: 'Here is an example of a toast component working...',
-        //     stack: 'testStack'
-        // });
+        toastService.showToast({
+            color: 'primary',
+            closeButton: {
+                color: 'primary',
+                invert: true,
+                label: 'Go there'
+            },
+            hide: false,
+            hideDelay: 4000,
+            message: 'Here is an example of a toast component working...',
+            stack: 'testStack'
+        });
     }
 
     return (
         <>
+            <Toast closeButton={{color: 'danger', label: 'Go there'}} message="Here is an example of a toast component working..." />
             <Button color="primary" mb={8} onClick={addToast}>Add Toast</Button>
-            <div>Teste...</div>
         </>
     )
 }

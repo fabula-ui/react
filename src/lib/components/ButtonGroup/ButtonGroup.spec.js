4abd5fc929943288ca4c61403b406139b2700c16
Bruno Poeta <brunovieirapoeta@gmail.com>
2020-06-29 11:06:41 -0300

    Added button group tests

---
+++ src/lib/components/ButtonGroup/ButtonGroup.spec.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+
+// Component
+import Button from '../Button/Button';
+import ButtonGroup from './ButtonGroup';
+
+describe('Button Group Component', () => {
+    it('Should create', () => {
+        const { container } = render(<ButtonGroup />);
+
+        expect(container).toBeTruthy();
+    });
+
+    it('Should have button as children', () => {
+        const { container } = render(<ButtonGroup><Button>Label</Button></ButtonGroup>);
+        const buttonElement = container.querySelector('.fab-button');
+
+        expect(buttonElement).toBeTruthy();
+    });
+});

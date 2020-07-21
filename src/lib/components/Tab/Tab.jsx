4902dd3dedd171df38f346d6cc5cabb73bad0f64
Bruno Poeta <bruno.poeta@toptal.com>
2020-07-21 18:56:10 -0300

    Refactored and reviewed tabs components

--- src/lib/components/Tab/Tab.jsx
+++ src/lib/components/Tab/Tab.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { css } from 'emotion';
+import PropTypes from 'prop-types';
 
 // Styles
 import TabStyles from '@fabula/core/styles/components/tab/tab';
@@ -25,6 +26,54 @@ const Tab = props => {
             {link && <a className="fab-tab" href={link} {...rest}>{label || children}</a>}
         </div>
     )
+}
+
+Tab.defaultProps = {
+    active: false,
+    activeColor: '',
+    activeFillColor: '',
+    activeTextColor: '',
+    clear: false,
+    color: '',
+    expand: false,
+    faded: false,
+    href: '',
+    inactiveFillColor: '',
+    inactiveTextColor: '',
+    invert: false,
+    layout: '',
+    link: '',
+    name: '',
+    outline: false,
+    rounded: false,
+    scope: '',
+    stacked: false,
+    target: '',
+    type: '',
+}
+
+Tab.propTypes = {
+    active: PropTypes.bool,
+    activeColor: PropTypes.string,
+    activeFillColor: PropTypes.string,
+    activeTextColor: PropTypes.string,
+    clear: PropTypes.bool,
+    color: PropTypes.string,
+    expand: PropTypes.bool,
+    faded: PropTypes.bool,
+    href: PropTypes.string,
+    inactiveFillColor: PropTypes.string,
+    inactiveTextColor: PropTypes.string,
+    invert: PropTypes.bool,
+    layout: PropTypes.string,
+    link: PropTypes.string,
+    name: PropTypes.string,
+    outline: PropTypes.bool,
+    rounded: PropTypes.bool,
+    scope: PropTypes.string,
+    stacked: PropTypes.bool,
+    target: PropTypes.string,
+    type: PropTypes.string,
 }
 
 export default Tab;

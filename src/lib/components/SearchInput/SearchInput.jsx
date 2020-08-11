38148e30559de73cc31898b1a52b819ebddad031
Bruno Poeta <bruno.poeta@toptal.com>
2020-08-11 20:13:00 -0300

    Minor changes on search input component

--- src/lib/components/SearchInput/SearchInput.jsx
+++ src/lib/components/SearchInput/SearchInput.jsx
@@ -5,13 +5,14 @@ import PropTypes from 'prop-types';
 import Button from '../Button/Button';
 import Component from '../Component/Component';
 import DropdownMenu from '../DropdownMenu/DropdownMenu';
+import InnerIcon from '../InnerIcon/InnerIcon';
 import Input from '../Input/Input';
 
 // Styles
 import SearchInputStyles from '@fabula/core/styles/components/search-input/search-input';
 
 const SearchInput = props => {
-    const { autocomplete, button, children, className, placeholder, ...rest } = props;
+    const { autocomplete, button, children, className, icon, placeholder, ...rest } = props;
     const [autocompleteIsOpen, setAutocompleIsOpen] = useState(false);
     const elRef = useRef(null);
 
@@ -28,8 +29,13 @@ const SearchInput = props => {
             <div ref={elRef}>
                 <div className="fab-search-input">
                     <Input iconStart={{ name: 'search' }} onFocus={() => handleFocus(true)} placeholder={placeholder || 'Search...'} {...rest}>
-                        {!!button && <Button color="primary" compact={true} {...button}>{!button.label && !button.icon && 'Search'}</Button>}
-                        {children}
+                        {!!button &&
+                            <Button color="primary" compact={true} {...button}>
+                                {!button.label && !button.icon && 'Search'}
+                                {!!button.icon && <InnerIcon icon={button.icon} parentProps={button} />}
+                            </Button>
+                        }
+                        {!button && children}
                     </Input>
 
                     {!!autocomplete && <DropdownMenu clickToClose={true} items={[{ button: true, label: 'Item 1' }, { button: true, label: 'Item 2' }, { button: true, label: 'Item 3' }]} onChange={setAutocompleIsOpen} open={autocompleteIsOpen}></DropdownMenu>}

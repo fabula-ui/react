9cfa053bbfe3e9f5a65ff410336887a6a9f8e9af
Bruno Poeta <brunovieirapoeta@gmail.com>
2020-11-11 20:13:02 -0300

    Added todo test to dropdown toggle

--- src/lib/components/DropdownToggle/DropdownToggle.spec.js
+++ src/lib/components/DropdownToggle/DropdownToggle.spec.js
@@ -25,10 +25,23 @@ describe('Dropdown Toggle Component', () => {
     });
 
     it('Should set color', () => {
-        const { container } = render(<DropdownToggle color="blue" icon="map-pin" label="Label" />);
+        const { container } = render(<DropdownToggle color="blue" label="Label" />);
         const buttonElement = container.querySelector('.fab-button');
         const buttonStyle = window.getComputedStyle(buttonElement);
 
         expect(buttonStyle.backgroundColor).toBe('blue');
     });
+
+    // TODO: fix this test
+    // it('Should be able to hide arrow', () => {
+    //     let component = render(<DropdownToggle arrow={true} label="Label" />);
+    //     let arrowElement = component.container.querySelector('.fab-dropdown-toggle__chevron');
+
+    //     expect(arrowElement).toBeTruthy();
+
+    //     component = render(<DropdownToggle arrow={false} label="Label" />)
+    //     arrowElement = component.container.querySelector('.fab-dropdown-toggle__chevron');
+
+    //     expect(arrowElement).toBeFalsy();
+    // });
 });
